57cd0279d7f661a14491efa2fa547ce131865c95
Daniel <daniel@pipelineworkforce.com>
2025-06-11 19:38:11 +0200

    remove typeorm

--- backend/api/src/tasks/tasks.service.ts
+++ backend/api/src/tasks/tasks.service.ts
@@ -1,32 +1,35 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Task } from './entities/task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { PrismaService } from 'src/common/prisma/prisma.service';
 
 @Injectable()
 export class TasksService {
   constructor(
-    @InjectRepository(Task)
-    private tasksRepository: Repository<Task>,
+    private readonly prismaService: PrismaService, 
   ) {}
 
-  create(createTaskDto: CreateTaskDto) {
-    const task = this.tasksRepository.create(createTaskDto);
-    return this.tasksRepository.save(task);
+  async create(createTaskDto: CreateTaskDto) {
+    const task = await this.prismaService.tasks.create({
+      data: createTaskDto,
+    });
+    return task;
   }
 
-  findAll() {
-    return this.tasksRepository.find({
-      relations: ['project', 'assignedTo'],
+  async findAll() {
+    return this.prismaService.tasks.findMany({
+      include: {
+        projects: true,
+      },
     });
   }
 
   async findOne(id: string) {
-    const task = await this.tasksRepository.findOne({
+    const task = await this.prismaService.tasks.findUnique({
       where: { id },
-      relations: ['project', 'assignedTo'],
+      include: {
+        projects: true,
+      },
     });
     if (!task) {
       throw new NotFoundException(`Task with ID ${id} not found`);
@@ -36,12 +39,17 @@ export class TasksService {
 
   async update(id: string, updateTaskDto: UpdateTaskDto) {
     const task = await this.findOne(id);
-    await this.tasksRepository.update(id, updateTaskDto);
+    await this.prismaService.tasks.update({
+      where: { id },
+      data: updateTaskDto,
+    });
     return this.findOne(id);
   }
 
   async remove(id: string) {
     const task = await this.findOne(id);
-    return this.tasksRepository.remove(task);
+    return this.prismaService.tasks.delete({
+      where: { id },
+    });
   }
-} 
+}
