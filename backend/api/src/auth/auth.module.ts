19224b372fd20fafce303853338e10f0e679b14b
Daniel <daniel@pipelineworkforce.com>
2025-06-11 19:38:11 +0200

    remove typeorm

--- backend/api/src/auth/auth.module.ts
+++ backend/api/src/auth/auth.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/users/entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './jwt-strategy';
@@ -11,7 +9,6 @@ import { JwtStrategy } from './jwt-strategy';
 @Module({
   imports: [
     UsersModule,
-    TypeOrmModule.forFeature([User]),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
