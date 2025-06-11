a9a673fd9dcf2901e920cb79ea47f0ddebe2be4b
Daniel <daniel@pipelineworkforce.com>
2025-06-11 18:03:15 +0200

    google auth

--- backend/api/src/auth/auth.service.ts
+++ backend/api/src/auth/auth.service.ts
@@ -10,6 +10,8 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
 
 @Injectable()
 export class AuthService {
@@ -17,7 +19,63 @@ export class AuthService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
     private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
   ) {}
+
+  async exchangeGoogleCode(code: string): Promise<string> {
+    const clientId = this.configService.get<string>('GOOGLE_CLIENT_ID');
+    const clientSecret = this.configService.get<string>('GOOGLE_CLIENT_SECRET');
+    const redirectUri = `${this.configService.get<string>('APP_URL')}${this.configService.get<string>('GOOGLE_CALLBACK_URL')}`;
+
+    const response = await axios.post(
+      'https://oauth2.googleapis.com/token',
+      null,
+      {
+        params: {
+          code,
+          client_id: clientId,
+          client_secret: clientSecret,
+          redirect_uri: redirectUri,
+          grant_type: 'authorization_code',
+        },
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    );
+
+    return response.data.access_token;
+  }
+
+  async getGoogleUserInfo(token: string): Promise<any> {
+    const response = await axios.get(
+      'https://www.googleapis.com/oauth2/v2/userinfo',
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+    return response.data;
+  }
+
+  async findOrCreateUser(userInfo: any): Promise<User> {
+    const existingUser = await this.userRepository.findOne({
+      where: { email: userInfo.email },
+    });
+
+    if (existingUser) {
+      return existingUser;
+    }
+
+    const newUser = this.userRepository.create({
+      name: userInfo.name,
+      email: userInfo.email,
+      password: '',
+    });
+
+    return await this.userRepository.save(newUser);
+  }
 
   async create(signUpDto: SignUpDto) {
     const { email, password, name } = signUpDto;
@@ -49,20 +107,4 @@ export class AuthService {
 
     return { access_token: token };
   }
-
-  // findAll() {
-  //   return `This action returns all auth`;
-  // }
-
-  // findOne(id: number) {
-  //   return `This action returns a #${id} auth`;
-  // }
-
-  // update(id: number, updateAuthDto: UpdateAuthDto) {
-  //   return `This action updates a #${id} auth`;
-  // }
-
-  // remove(id: number) {
-  //   return `This action removes a #${id} auth`;
-  // }
 }
