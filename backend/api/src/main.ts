83ff4bd56db93c447c9f363d678dbe9c8dd889da
Daniel <daniel@pipelineworkforce.com>
2025-06-23 18:36:09 +0200

    integrate Swagger setup and add decorators to Dtos for API docs

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -2,25 +2,36 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+  const config = new DocumentBuilder()
+    .setTitle('Pipeline')
+    .setDescription('Pipleline')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
   // Enable CORS
   app.enableCors();
 
   // Set up global response interceptor
   app.useGlobalInterceptors(new ResponseInterceptor());
-  
+
   // Enable validation
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   // Set global prefix
   app.setGlobalPrefix('api');
 
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
