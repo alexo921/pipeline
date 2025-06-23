52c08cd73f3f8fd97fe5f456670760a3a7bc379b
Daniel <daniel@pipelineworkforce.com>
2025-06-23 18:23:41 +0200

    demo api is created for inserting a job

---
+++ backend/api/src/job/job.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { JobService } from './job.service';
+import { CreateJobDto } from './dto/create-job.dto';
+import { UpdateJobDto } from './dto/update-job.dto';
+
+@Controller('job')
+export class JobController {
+  constructor(private readonly jobService: JobService) {}
+
+  @Post()
+  dummyCreate() {
+    return this.jobService.create();
+  }
+
+  @Get()
+  findAll() {
+    return this.jobService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.jobService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateJobDto: UpdateJobDto) {
+    return this.jobService.update(+id, updateJobDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.jobService.remove(+id);
+  }
+}
