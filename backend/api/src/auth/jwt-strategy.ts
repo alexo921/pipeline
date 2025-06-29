4eaeb1a6d58d07b066599c99c333c2aa850f43e6
Daniel <daniel@pipelineworkforce.com>
2025-06-29 12:42:19 +0200

    setup cookie parser for auth check and logged in users

--- backend/api/src/auth/jwt-strategy.ts
+++ backend/api/src/auth/jwt-strategy.ts
@@ -3,11 +3,18 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
 
+interface RequestWithCookies extends Request {
+  cookies: Record<string, string>;
+}
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private configService: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: RequestWithCookies) => req?.cookies?.access_token || null, // HttpOnly cookie
+        ExtractJwt.fromAuthHeaderAsBearerToken(), // Bearer token
+      ]),
       secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
