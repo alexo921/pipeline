db92b057c603770cbc901b7bb512df908eed1fcb
Daniel <daniel@pipelineworkforce.com>
2025-06-29 12:48:42 +0200

    create auth context for logged in user and refactor for base layout component

---
+++ frontend/web-dashboard/app/contexts/AuthContext.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { createContext, useContext, useEffect, useState } from 'react';
+
+type User = {
+  name: string;
+  email: string;
+};
+
+type AuthContextType = {
+  user: User | null;
+  setUser: (user: User | null) => void;
+  refreshUser: () => Promise<void>;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const refreshUser = async () => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/profile`,
+        {
+          credentials: "include",
+        }
+      );
+      if (!res.ok) return setUser(null);
+      const data = await res.json();
+      setUser(data);
+    } catch {
+      setUser(null);
+    }
+  };
+
+  useEffect(() => {
+    refreshUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, refreshUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error('useAuth must be used within AuthProvider');
+  return context;
+};
