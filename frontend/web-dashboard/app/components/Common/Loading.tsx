7e05f563d402ab36b8321f9a0f4d643a3618ac07
Daniel <daniel@pipelineworkforce.com>
2025-07-03 14:58:51 +0200

    Login modal ui update according to design

---
+++ frontend/web-dashboard/app/components/Common/Loading.tsx
@@ -0,0 +1,9 @@
+const loading = () => {
+    return (
+        <div className="min-h-screen bg-[#F4F4F4] font-baloo flex items-center justify-center flex-col">
+            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-[#2466D0] mx-auto mb-4"></div>
+        </div>
+    );
+};
+
+export default loading;
--- frontend/web-dashboard/app/user-dashboard/page.tsx
+++ frontend/web-dashboard/app/user-dashboard/page.tsx
@@ -2,34 +2,28 @@
 
 import React from "react";
 import "../styles/brand.css";
+import DashboardCard from "../components/dashboard-layout/card";
 
 export default function UserDashboardPage() {
+  const cardsData = [
+    { number: 12, label: "Open Jobs" },
+    { number: 4, label: "New Matches" },
+    { number: 28, label: "Interest Applicant" },
+  ];
   return (
     <div className="p-6 w-full bg-[#F4F5FF] min-h-screen">
       <div className="max-w-6xl mx-auto">
         <h1 className="text-2xl font-bold mb-8 text-gray-900">
           Welcome to Your Dashboard
         </h1>
-
-        {/* Cards Row */}
         <div className="flex flex-wrap gap-6 justify-start">
-          {/* Card 1 */}
-          <div className="bg-white rounded-2xl shadow-sm w-[200px] h-[200px] flex flex-col justify-center items-center text-center">
-            <div className="baloo-number text-[#0071DC] mb-1">12</div>
-            <div className="dashboard-label">Open Jobs</div>
-          </div>
-
-          {/* Card 2 */}
-          <div className="bg-white rounded-2xl shadow-sm w-[200px] h-[200px] flex flex-col justify-center items-center text-center">
-            <div className="baloo-number text-[#0071DC] mb-1">4</div>
-            <div className="dashboard-label">New Matches</div>
-          </div>
-
-          {/* Card 3 */}
-          <div className="bg-white rounded-2xl shadow-sm w-[200px] h-[200px] flex flex-col justify-center items-center text-center">
-            <div className="baloo-number text-[#0071DC] mb-1">28</div>
-            <div className="dashboard-label">Interest Applicant</div>
-          </div>
+          {cardsData.map((card, index) => (
+            <DashboardCard
+              key={index}
+              number={card.number}
+              label={card.label}
+            />
+          ))}
         </div>
       </div>
     </div>
