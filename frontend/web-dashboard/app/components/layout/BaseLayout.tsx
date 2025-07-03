a4ab5709e3914c8ca1ce0a576df2a51dc355df57
Ubuntu <ubuntu@ip-172-31-30-214.us-east-2.compute.internal>
2025-07-03 19:16:46 +0000

    Fixed

--- frontend/web-dashboard/app/components/layout/BaseLayout.tsx
+++ frontend/web-dashboard/app/components/layout/BaseLayout.tsx
@@ -7,6 +7,7 @@ import AuthModal from "../AuthModal";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import BaseAuthModal from "../BaseAuthModal";
+import { useAuth } from "../../contexts/AuthContext";
 
 interface BaseLayoutProps {
   children: React.ReactNode;
